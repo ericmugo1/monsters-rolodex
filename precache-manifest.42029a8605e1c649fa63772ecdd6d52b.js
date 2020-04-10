5d3390c66e4fe9ad6e83009f7f362e8c7dd4a38a
ericmugo1 <35067862+ericmugo1@users.noreply.github.com>
2020-04-10 21:31:22 +0200

    Updates

---
+++ precache-manifest.42029a8605e1c649fa63772ecdd6d52b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "663331ea9fe7df01217fc685d92f08a3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "05bcc59149212a0e0a0f",
+    "url": "/monsters-rolodex/static/css/main.ebbc4f76.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05bcc59149212a0e0a0f",
+    "url": "/monsters-rolodex/static/js/main.f739f29c.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
